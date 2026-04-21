8419aff9e01b808d1157b4cb8e1c387f54637bf2
Danish Al Fayyadh Sunarta <danishalfay.711@gmail.com>
2026-04-21 16:02:13 +0700

    init

---
+++ backend/chaoticTower-server/src/main/java/com/alfa/backend/ChaoticTowerServerApplication.java
@@ -0,0 +1,13 @@
+package com.alfa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaoticTowerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaoticTowerServerApplication.class, args);
+	}
+
+}
---
+++ backend/chaoticTower-server/src/test/java/com/alfa/backend/ChaoticTowerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaoticTowerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
